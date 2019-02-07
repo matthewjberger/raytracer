71054df1293e30a62131db050403a0befa9c136a
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-02-07 08:16:32 -0800

    Complete chapter 4.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use crate::vec::{Ray, Vec3};
 use std::fs::File;
 use std::io::Write;
-use crate::vec::{Vec3, Ray};
 
 mod vec;
 
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+    let a = r.direction.dot(r.direction);
+    let b = 2.0 * oc.dot(r.direction);
+    let c = oc.dot(oc) - radius.powi(2);
+    let discriminant = b.powi(2) - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn color(r: Ray) -> Vec3 {
+    if hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, &r) {
+        return Vec3(0.0, 1.0, 0.0);
+    };
     let unit_direction = r.direction.to_unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
@@ -28,10 +40,17 @@ fn main() {
             let u = x as f32 / width as f32;
             let v = y as f32 / height as f32;
 
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
 
             let color = color(r) * 255.99;;
-            writeln!(output, "{} {} {}", color.x() as i32, color.y() as i32, color.z() as i32).unwrap();
+            writeln!(
+                output,
+                "{} {} {}",
+                color.x() as i32,
+                color.y() as i32,
+                color.z() as i32
+            )
+            .unwrap();
         }
     }
 }
