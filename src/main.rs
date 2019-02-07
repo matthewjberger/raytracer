b5d83892f0d66c6ea977d3f371ce2e6d054de977
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-02-06 22:54:08 -0800

    Complete chapter 3.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::fs::File;
 use std::io::Write;
+use crate::vec::{Vec3, Ray};
 
-mod vec3;
+mod vec;
+
+fn color(r: Ray) -> Vec3 {
+    let unit_direction = r.direction.to_unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+}
 
 fn main() {
     let filename = "output.ppm";
@@ -11,12 +18,20 @@ fn main() {
     let height = 100;
     writeln!(output, "P3\n{} {}\n255", width, height).unwrap();
 
+    let lower_left_corner = Vec3(-2.0, -1.0, -1.0);
+    let horizontal = Vec3(4.0, 0.0, 0.0);
+    let vertical = Vec3(0.0, 2.0, 0.0);
+    let origin = Vec3(0.0, 0.0, 0.0);
+
     for y in (0..height).rev() {
         for x in 0..width {
-            let red = (f64::from(x) / f64::from(width)) * 255.99;
-            let green = (f64::from(y) / f64::from(height)) * 255.99;
-            let blue = 0.2 * 255.99;
-            writeln!(output, "{} {} {}", red as i32, green as i32, blue as i32).unwrap();
+            let u = x as f32 / width as f32;
+            let v = y as f32 / height as f32;
+
+            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+
+            let color = color(r) * 255.99;;
+            writeln!(output, "{} {} {}", color.x() as i32, color.y() as i32, color.z() as i32).unwrap();
         }
     }
 }
--- src/vec.rs
+++ src/vec.rs
@@ -81,3 +81,18 @@ impl Div<f32> for Vec3 {
         (1.0 / scalar) * self
     }
 }
+
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn point_at_parameter(self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
