fdb0083b96c01ee1e26a10e3e933f7fb9d9ba738
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-02-08 15:20:10 -0800

    Add diffuse lighting with lambertian reflectance and apply gamma correction using gamma 2.

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,28 @@ mod camera;
 mod model;
 mod vec;
 
+fn random_in_unit_sphere() -> Vec3 {
+    loop {
+        let p =
+            2.0 * Vec3(
+                rand::thread_rng().gen_range(0.0, 1.0),
+                rand::thread_rng().gen_range(0.0, 1.0),
+                rand::thread_rng().gen_range(0.0, 1.0),
+            ) - Vec3(1.0, 1.0, 1.0);
+        if p.squared_length() >= 1.0 {
+            return p;
+        };
+    }
+}
+
 fn color(r: Ray, world: &[Box<Model>]) -> Vec3 {
     const WHITE: Vec3 = Vec3(1.0, 1.0, 1.0);
     const SKY_BLUE: Vec3 = Vec3(0.5, 0.7, 1.0);
+
     let interval = TimeInterval::new(0.0, i32::max_value() as f32);
     if let Some(hit) = world.hit(&r, &interval) {
-        return 0.5 * (hit.normal + 1.0);
+        let target = hit.p + hit.normal + random_in_unit_sphere();
+        return 0.5 * color(Ray::new(hit.p, target - hit.p), &world);
     }
     let unit_direction = r.direction.as_unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
@@ -55,7 +71,11 @@ fn main() {
                 blended_color = blended_color + color(r, &world);
             }
             blended_color = blended_color / (samples_per_pixel as f32);
-            let final_color = blended_color * 255.99;
+            let final_color = Vec3(
+                blended_color.x().sqrt(),
+                blended_color.y().sqrt(),
+                blended_color.z().sqrt(),
+            ) * 255.99;
 
             writeln!(
                 output,
