143d572f4fed508d61cb4ca945406b5eba390f4c
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-02-13 07:22:37 -0800

    Break sphere scene out into separate function.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+extern crate image;
+extern crate num;
+extern crate num_cpus;
 extern crate rand;
+extern crate threadpool;
+
+use image::{ImageBuffer, Pixel, Rgb};
+use num::complex::Complex;
+use std::sync::mpsc::{channel, RecvError};
+use threadpool::ThreadPool;
 
 use rand::Rng;
 use std::fs::File;
 use std::io::Write;
+use std::vec::Vec;
 
 use crate::camera::*;
 use crate::material::*;
@@ -16,6 +26,39 @@ mod vec;
 
 fn drand48() -> f32 {
     rand::thread_rng().gen_range(0.0, 1.0)
+}
+
+fn sphere_scene() -> Vec<Box<Model>> {
+    vec![
+        Box::new(Sphere::new(
+            Vec3(0.0, 0.0, -1.0),
+            0.5,
+            Box::new(Lambertian::new(Vec3(0.1, 0.2, 0.5))),
+        )),
+        Box::new(Sphere::new(
+            Vec3(0.0, -100.5, -1.0),
+            100.0,
+            Box::new(Lambertian::new(Vec3(0.8, 0.8, 0.0))),
+        )),
+        Box::new(Sphere::new(
+            Vec3(1.0, 0.0, -1.0),
+            0.5,
+            Box::new(Metal::new(Vec3(0.8, 0.6, 0.2), 0.3)),
+        )),
+        Box::new(Sphere::new(
+            Vec3(-1.0, 0.0, -1.0),
+            0.5,
+            Box::new(Dielectric::new(1.5)),
+        )),
+        Box::new(Sphere::new(
+            Vec3(-1.0, 0.0, -1.0),
+            -0.45,
+            Box::new(Dielectric::new(1.5)),
+        )),
+    ]
+    .into_iter()
+    .map(|s| s as Box<Model>)
+    .collect()
 }
 
 fn random_scene() -> std::vec::Vec<std::boxed::Box<model::Model>> {
@@ -108,40 +151,12 @@ fn main() {
     let filename = "output.ppm";
     let mut output = File::create(filename).unwrap();
 
-    let width = 800;
-    let height = 400;
+    let width = 200;
+    let height = 100;
     let samples_per_pixel = 100;
     writeln!(output, "P3\n{} {}\n255", width, height).unwrap();
 
-    // let world: Vec<Box<Model>> = vec![
-    //     Box::new(Sphere::new(
-    //         Vec3(0.0, 0.0, -1.0),
-    //         0.5,
-    //         Box::new(Lambertian::new(Vec3(0.1, 0.2, 0.5))),
-    //     )),
-    //     Box::new(Sphere::new(
-    //         Vec3(0.0, -100.5, -1.0),
-    //         100.0,
-    //         Box::new(Lambertian::new(Vec3(0.8, 0.8, 0.0))),
-    //     )),
-    //     Box::new(Sphere::new(
-    //         Vec3(1.0, 0.0, -1.0),
-    //         0.5,
-    //         Box::new(Metal::new(Vec3(0.8, 0.6, 0.2), 0.3)),
-    //     )),
-    //     Box::new(Sphere::new(
-    //         Vec3(-1.0, 0.0, -1.0),
-    //         0.5,
-    //         Box::new(Dielectric::new(1.5)),
-    //     )),
-    //     Box::new(Sphere::new(
-    //         Vec3(-1.0, 0.0, -1.0),
-    //         -0.45,
-    //         Box::new(Dielectric::new(1.5)),
-    //     )),
-    // ];
-
-    let world = random_scene();
+    let world = sphere_scene();
 
     let look_from = Vec3(13.0, 2.0, 3.0);
     let look_at = Vec3(0.0, 0.0, 0.0);
